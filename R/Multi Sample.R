44f9a145e10cd492825e97dc464c15e466ee2201
Tanner <tanner.schubert@slu.edu>
2017-11-11 21:38:51 -0600

    It works! Also added documentation.

--- R/Multi Sample.R
+++ R/Multi Sample.R
@@ -1,14 +1,27 @@
+#' Multivariable Random Samples
+#' 
+#' Description
+#' 
+#' @param PDF a function to be sampled from
+#' @param N number of attempted samples
+#' @param a lower bound of the x support of PDF
+#' @param b upper bound of the y support of PDF
+#' 
+#' @return a dataframe of samples from PDF
+#' @export
+#' 
+#' @examples 
+#' ExPDF <- function(x) x+y
+#' mvrs(ExPDF, 100, 0, 1, 0, 1)
 mvrs <- function(PDF, N, a, b, c, d){
-  fxmax <- optimize(PDF, c(a,b), lower = a, upper = b,
+  fxmax <- optimize(PDF, c(a,b), y=0, lower = a, upper = b,
                    maximum = TRUE)
-  fymax <- optimize(PDF, c(c,d), lower = c, upper = d,
+  fymax <- optimize(PDF, c(c,d), x=0, lower = c, upper = d,
                     maximum = TRUE)
-  fxymax <- PDF(fxmax, fymax)
+  fxymax <- PDF(fxmax$maximum, fymax$maximum)
   xsamp <- runif(N, a, b)
   ysamp <- runif(N, c, d)
   zsamp <- runif(N, 0, fxymax)
-  estPDF <- if(zsamp<PDF(xsamp, ysamp)) {c(xsamp, ysamp)} else(c(NA,NA))
-  Samples <- data.frame(t(estPDF))
+  estPDF <- data.frame(x=ifelse(xsamp[zsamp<PDF(xsamp,ysamp)], xsamp, NA), y=ifelse(ysamp[zsamp<PDF(xsamp,ysamp)], ysamp, NA))
+  return(estPDF)
 }
-
-exPDF <- function(x,y) {ifelse(0<x & x<1, x, 0) & ifelse(0<y & y<1, y, 0); x+y}
